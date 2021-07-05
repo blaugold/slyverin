3a038b9f1ac51dad50614dd44ec7ef0d5fd383f2
Gabriel Terwesten <gabriel@terwesten.net>
2021-07-05 21:06:45 +0200

    chore: update example live demo

---
+++ tool/build_example_live_demo.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$DIR/.."
+EXAMPLE_DIR="$PROJECT_DIR/example"
+EXAMPLE_BUILD_DIR="$PROJECT_DIR/example/build/web"
+EXAMPLE_LIVE_DEMO_DIR="$PROJECT_DIR/docs/example_dist"
+
+cd "$EXAMPLE_DIR"
+
+flutter build web --pwa-strategy none
+
+rm -rf "$EXAMPLE_LIVE_DEMO_DIR"
+mkdir -p "$EXAMPLE_LIVE_DEMO_DIR"
+cp -R "$EXAMPLE_BUILD_DIR/" "$EXAMPLE_LIVE_DEMO_DIR"
